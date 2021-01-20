aa8b773f8f6bc6c09a4d5d63ceb98358d3ad62e6
FAnatole <fedoto4kin@gmail.com>
2021-01-20 16:18:04 +0300

    Change global app name

--- app/common/config/main.php
+++ app/common/config/main.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
+    'name' => 'My Crazy Guestbook',
+    'aliases' => [        '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
